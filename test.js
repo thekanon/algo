ce199de6d324691bb69326da457fac8a3dc35737
leedo <dlengjs123@gmail.com>
2022-09-04 23:22:30 +0900

    푼듯
    Co-authored-by: leedo <dlengjs123@gmail.com>

--- test.js
+++ test.js
@@ -32,61 +32,32 @@ i+(i+len-j) =
 4-7 = -3
 
 1, 2, 1, 2,	1, 10, 1, 2, 1, 2, 1, 2, 1, 10, 1, 2
+
+
+
 */
 function solution(queue1, queue2) {
-    var answer = -1;
-
     const arr = [...queue1, ...queue2, ...queue1, ...queue2]
-    let eq = (queue1.reduce((acc, cur) => acc + cur, 0) + queue2.reduce((acc, cur) => acc + cur, 0))
-    let total = 0
-
-    //function
-    const check = (arr1,arr2) => {
-        queue1 = [...queue1, ...queue2]
-        queue2 = [...queue2, ...queue1]
-        console.log(queue1+"\n"+queue2)
-        console.log("")
-        console.log(arr1+"\n"+arr2)
-        let r = 0
-        for(let i=0; i<queue1.length; i++){
-            if(JSON.stringify(queue1.slice(i,i+arr1.length)) === JSON.stringify(arr1)){
-                r+=i
-                break;
-            }
-        }
-        for(let i=0; i<queue2.length; i++){
-            if(JSON.stringify(queue2.slice(i,i+arr2.length)) === JSON.stringify(arr2)){
-                r+=i
-                break
-            }
-        }
-        return r
-    }
-    
-
-
-
-    if(eq % 2 === 1){
+    let eq = arr.reduce((acc, cur) => acc + cur, 0)
+    let total = queue1.reduce((acc, cur) => acc + cur, 0)
+   
+    if(eq % 4 === 1){
         return -1
     }
-    let len1 = queue1.length
-    let len2 = queue2.length
-    for(let i = 0; i < arr.length; i++){
-        let totalArr = []
-        for(let j = i; j < i+arr.length/2; j++){
-            if(total === eq/2){
-                answer = i - len1 + 1
-                return check(totalArr,arr.slice(j,j+((arr.length/2)-totalArr.length)))
-            } else if(total > eq/2){
-                total = 0
-                totalArr = []
-                break
-            }
-            total += arr[j]
-            totalArr.push(arr[j])
+
+    for(let i=queue1.length, j=0; i < arr.length;){
+        console.log(total,eq/4)
+        if( total<eq/4 ){
+            total += arr[i]
+            i++
+        }else if(total > eq/4){
+            total -= arr[j]
+            j++
+        } else if(total === eq/4){
+            return i+j-queue1.length
         }
     }
-    return answer;
+    return -1;
 }
 let queue1 = [3, 2, 7, 2]
 let queue2 = [4, 6, 5, 1]
